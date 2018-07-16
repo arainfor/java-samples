f9e2c937f40d2060035083b4d7b725a2e1dedef3
Alan Rainford <arainfor@gmail.com>
2018-07-16 18:28:53 -0500

    Initial commit

---
+++ src/main/java/org/arainfor/App.java
@@ -0,0 +1,11 @@
+package org.arainfor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/arainfor/Person.java
@@ -0,0 +1,20 @@
+package org.arainfor;
+
+class Person {
+    String firstName;
+    String lastName;
+
+    public String getLastName() {
+        return lastName;
+    };
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
